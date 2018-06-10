ccdb5d669501cef88ef23bf9272011a751c23290
Glenn Keates <glennrkeates@gmail.com>
2018-06-10 18:24:09 +0100

    add qrunnables and ffmpeg commands

--- converter.py
+++ converter.py
@@ -11,6 +11,49 @@ import tempfile
 
 from PyQt5 import QtCore, QtGui, QtWidgets
 from qt_mainwindow import Ui_MainWindow
+from downloaddialog import Ui_DownloadDialog
+
+
+class MergeAudioVideo(QtCore.QRunnable):
+    def __init__(self, audio_path, video_path, output_path, vidcodec):
+        QtCore.QRunnable.__init__(self)
+        self.audio_path = audio_path
+        self.video_path = video_path
+        self.output_path = output_path
+        self.vidcodec = vidcodec
+        self.codecs = {
+            'x264': 'libx264'
+        }
+
+
+    def run(self):
+        os.system('ffmpeg -i '+ self.audio_path + ' -i ' + self.video_path + ' -y -acodec libfdk_aac -b:a 160k -vcodec' + self.codecs[self.vidcodec] +' -preset fast -crf 20 ' + self.output_path)
+
+class ConvertAudio(QtCore.QRunnable):
+    def __init__(self, audio_path, output_path, br):
+        QtCore.QRunnable.__init__()
+        self.audio_path = audio_path
+        self.output_path = output_path
+        self.br = br
+
+    def run(self):
+        os.system('ffmpeg -i ' + self.audio_path + ' -y -codec:a libmp3lame -ac 2 -ar 44100 -ab 160k ' + self.output_path)
+
+class DownloadFileThread(QtCore.QRunnable):
+    def __init__(self, yt, itag, directory, name=None):
+        QtCore.QRunnable.__init__(self)
+        self.yt = yt
+        self.itag = itag
+        self.directory = directory
+        self. name = name
+
+
+
+    def run(self):
+        print('downloading ' + str(self.itag))
+        stream = self.yt.streams.get_by_itag(self.itag)
+        stream.download(self.directory, self.name)
+
 
 
 class YTConverter(Ui_MainWindow):
@@ -30,6 +73,15 @@ class YTConverter(Ui_MainWindow):
         self.console.moveCursor(QtGui.QTextCursor.End)
 
 
+
+    def finishdownload(self):
+        """
+        Show the message that fetching posts is done.
+        Disable Stop button, enable the Start one and reset progress bar to 0
+        """
+        self.download.setEnabled(True)
+        QtGui.QMessageBox.information(self, "Done!", "Finished Downloading!!")
+
     def processurlpressed(self):
         url = self.urlBox.text()
         if not url:
@@ -45,13 +97,12 @@ class YTConverter(Ui_MainWindow):
                 self.consolewrite("URL not valid\n")
         if self.fileType.currentText() == 'Video':
             try:
-                self.vids = self.yt.streams.filter(adaptive=True).filter(only_video=True).asc().all()
+                self.vids = self.yt.streams.filter(adaptive=True).filter(subtype='webm').filter(only_video=True).asc().all()
                 self.fileTable.setRowCount(0)
-                self.fileTable.setColumnCount(6)
+                self.fileTable.setColumnCount(5)
                 self.fileTable.setHorizontalHeaderLabels(
-                    ["Download?","iTag","Original Format","Resolution","Frame Rate","Filesize"])
+                    ["Download?","iTag","Resolution","Frame Rate","Filesize"])
                 for stream in self.vids:
-                    print('run')
                     rowPosition = self.fileTable.rowCount()
                     self.fileTable.insertRow(rowPosition)
                     chkBoxItem = QtWidgets.QTableWidgetItem()
@@ -59,10 +110,9 @@ class YTConverter(Ui_MainWindow):
                     chkBoxItem.setCheckState(QtCore.Qt.Unchecked)
                     self.fileTable.setItem(rowPosition, 0, chkBoxItem)
                     self.fileTable.setItem(rowPosition, 1, QtWidgets.QTableWidgetItem(stream.itag))
-                    self.fileTable.setItem(rowPosition, 2, QtWidgets.QTableWidgetItem(stream.mime_type.strip('video/')))
-                    self.fileTable.setItem(rowPosition, 3, QtWidgets.QTableWidgetItem(stream.resolution))
-                    self.fileTable.setItem(rowPosition, 4, QtWidgets.QTableWidgetItem(str(stream.fps)))
-                    self.fileTable.setItem(rowPosition, 5, QtWidgets.QTableWidgetItem(
+                    self.fileTable.setItem(rowPosition, 2, QtWidgets.QTableWidgetItem(stream.resolution))
+                    self.fileTable.setItem(rowPosition, 3, QtWidgets.QTableWidgetItem(str(stream.fps)))
+                    self.fileTable.setItem(rowPosition, 4, QtWidgets.QTableWidgetItem(
                         size(stream.filesize, system=verbose)))
 
             except:
@@ -70,13 +120,12 @@ class YTConverter(Ui_MainWindow):
 
         elif self.fileType.currentText() == 'Audio':
             try:
-                self.vids = self.yt.streams.filter(adaptive=True).filter(only_audio=True).asc().all()
+                self.vids = self.yt.streams.filter(adaptive=True).filter(subtype='webm').filter(only_audio=True).asc().all()
                 self.fileTable.setRowCount(0)
-                self.fileTable.setColumnCount(5)
+                self.fileTable.setColumnCount(4)
                 self.fileTable.setHorizontalHeaderLabels(
-                    ["Download?","iTag","Original Format","Bit Rate", "Filesize"])
+                    ["Download?","iTag","Bit Rate", "Filesize"])
                 for stream in self.vids:
-                    print('run')
                     rowPosition = self.fileTable.rowCount()
                     self.fileTable.insertRow(rowPosition)
                     chkBoxItem = QtWidgets.QTableWidgetItem()
@@ -84,9 +133,8 @@ class YTConverter(Ui_MainWindow):
                     chkBoxItem.setCheckState(QtCore.Qt.Unchecked)
                     self.fileTable.setItem(rowPosition, 0, chkBoxItem)
                     self.fileTable.setItem(rowPosition, 1, QtWidgets.QTableWidgetItem(stream.itag))
-                    self.fileTable.setItem(rowPosition, 2, QtWidgets.QTableWidgetItem(stream.mime_type.strip('video/')))
-                    self.fileTable.setItem(rowPosition, 3, QtWidgets.QTableWidgetItem(stream.abr))
-                    self.fileTable.setItem(rowPosition, 4,
+                    self.fileTable.setItem(rowPosition, 2, QtWidgets.QTableWidgetItem(stream.abr))
+                    self.fileTable.setItem(rowPosition, 3,
                                            QtWidgets.QTableWidgetItem(size(stream.filesize, system=verbose)))
 
             except:
@@ -99,32 +147,80 @@ class YTConverter(Ui_MainWindow):
         self.dlDirectory.setText(file)
 
     def downloadpressed(self):
+        self.download.setEnabled(True)
         checked_list = []
+        filename = self.filename.text()
         for i in range(self.fileTable.rowCount()):
             if self.fileTable.item(i, 0).checkState() == QtCore.Qt.Checked:
                 checked_list.append(self.fileTable.item(i, 1).text().strip('iTag: '))
-        processes = []
-        directory = self.dlDirectory.text()
-        for i in checked_list:
-            print(i)
-            p = mp.Process(target=download, args=(self.yt, int(i), self.tempdir, None))
-            processes.append(p)
-        p = mp.Process(target=download, args=(self.yt, 251, self.tempdir, None))
-        processes.append(p)
+        if self.fileType.currentText() == 'Video':
+            try:
+                self.threadpool = QtCore.QThreadPool()
+                print("Multithreading with maximum %d threads" % self.threadpool.maxThreadCount())
+                if filename:
+                    for i in checked_list:
+                        p = DownloadFileThread(self.yt, int(i), self.tempdir, str(filename + ' - ' + self.yt.streams.get_by_itag(int(i)).resolution))
+                        self.threadpool.start(p)
+                    p = DownloadFileThread(self.yt, 251, self.tempdir, filename + ' - audio')
+                    self.threadpool.start(p)
+                else:
+                    for i in checked_list:
+                        p = DownloadFileThread(self.yt, int(i), self.tempdir,  str(self.yt.title + ' - ' + self.yt.streams.get_by_itag(int(i)).resolution))
+                        self.threadpool.start(p)
+                    p = DownloadFileThread(self.yt, 251, self.tempdir, self.yt.title + ' - audio')
+                    self.threadpool.start(p)
+            except:
+                traceback.print_exc()
+        elif self.fileType.currentText() == 'Audio':
+            try:
+                self.threadpool = QtCore.QThreadPool()
+                print("Multithreading with maximum %d threads" % self.threadpool.maxThreadCount())
+                if filename:
+                    for i in checked_list:
+                        p = DownloadFileThread(self.yt, int(i), self.tempdir, str(filename + ' - ' + self.yt.streams.get_by_itag(int(i)).abr))
+                        self.threadpool.start(p)
+                else:
+                    for i in checked_list:
+                        p = DownloadFileThread(self.yt, int(i), self.tempdir,  str(self.yt.title + ' - ' + self.yt.streams.get_by_itag(int(i)).abr))
+                        self.threadpool.start(p)
+            except:
+                traceback.print_exc()
+        self.threadpool.waitForDone()
+        self.threadpool = QtCore.QThreadPool()
+        try:
+            if self.fileType.currentText() == 'Video':
+                if filename:
+                    for i in checked_list:
+                        p = MergeAudioVideo(
+                            self.tempdir + filename + ' - audio.webm',
+                            self.tempdir + filename + ' - ' + self.yt.streams.get_by_itag(int(i)).abr + '.webm',
+                            self.dlDirectory.text() + filename + ' - ' + self.yt.streams.get_by_itag(int(i)).abr + '.mp4',
+                            self.fileFormat.currentText()
+                        )
+                        self.threadpool.start(p)
+                else:
+                    for i in checked_list:
+                        p = MergeAudioVideo(
+                            self.tempdir + filename + ' - audio.webm',
+                            self.tempdir + self.yt.title + ' - ' + self.yt.streams.get_by_itag(int(i)).abr + '.webm',
+                            self.dlDirectory.text() + self.yt.title + ' - ' + self.yt.streams.get_by_itag(int(i)).abr + '.mp4',
+                            str(self.yt.streams.get_by_itag(int(i)).abr).strip('bps')
+                        )
+                        self.threadpool.start(p)
+            elif self.fileType.currentText() == 'Audio':
+                if filename:
+                    for i in checked_list:
+                        p = ConvertAudio(
+                            self.tempdir + filename + ' - ' + self.yt.streams.get_by_itag(int(i)).abr + '.webm',
+                            self.dlDirectory.text() + filename + ' - ' + self.yt.streams.get_by_itag(int(i)).abr + '.mp3',
+                            str(self.yt.streams.get_by_itag(int(i)).abr).strip('bps')
+                        )
+                        self.threadpool.start(p)
+        except:
+            traceback.print_exc()
+        self.threadpool.waitForDone()
+        self.finishdownload()
 
-        print(processes)
-        for x in processes:
-            x.start()
-        for x in processes:
-            x.join()
-
-
-
-def download(yt, itag, directory, name):
-    print('downloading ' + str(itag))
-    stream = yt.streams.get_by_itag(itag)
-    stream.download(directory)
-    print('finished downloading ' + str(itag))
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
