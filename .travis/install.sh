7edd01fd669c8fce0defcbc8a4fc4d36158a4f73
GLENN-LAPTOP\glenn <glennrkeates@gmail.com>
2018-07-08 22:14:08 +0100

    make install.sh executable

--- .travis/install.sh
+++ .travis/install.sh
@@ -4,8 +4,10 @@ if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
     brew update
     brew outdates xctool || brew upgrade xctool
     brew install python36
+    sudo pip3 install pyqt5 moviepy requests pytube movieio
 else
     sudo apt get install python36
+    sudo pip install pyqt5 moviepy requests pytube movieio
 fi
 
-sudo pip3 install pyqt5 moviepy requests pytube movieio
+
