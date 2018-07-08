16d41e182ec116ff0162bd0a0d65f5184109f074
GLENN-LAPTOP\glenn <glennrkeates@gmail.com>
2018-07-08 22:28:46 +0100

    add pyinstaller

--- .travis/install.sh
+++ .travis/install.sh
@@ -4,11 +4,11 @@ if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
     brew update
     brew outdates xctool || brew upgrade xctool
     brew install python36
-    sudo pip3 install pyqt5 moviepy requests pytube imageio
+    sudo pip3 install pyqt5 moviepy requests pytube imageio pyinstaller
 else
     sudo apt get install python36
     sudo apt-get install python3-pyqt5
-    sudo pip install requests pytube imageio
+    sudo pip install requests pytube imageio pyinstaller
 fi
 
 
