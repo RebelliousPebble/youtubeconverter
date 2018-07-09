750360187af9628137899373671d69529f1a3972
GLENN-LAPTOP\glenn <glennrkeates@gmail.com>
2018-07-09 10:51:43 +0100

    add custom moviepy

--- .travis/install.sh
+++ .travis/install.sh
@@ -4,7 +4,7 @@ if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
     brew update
     brew outdates xctool || brew upgrade xctool
     brew upgrade python
-    pip3 install pyqt5 requests pytube imageio pyinstaller git+https://github.com/RebelliousPebble/moviepy.git
+    pip3 install --user pyqt5 requests pytube imageio pyinstaller git+https://github.com/RebelliousPebble/moviepy.git
 else
     sudo apt-get install python3 python3-pip
     pip3 install requests pytube imageio pyinstaller pyqt5 sip git+https://github.com/RebelliousPebble/moviepy.git
