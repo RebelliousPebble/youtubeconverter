596eacb412c058797b0a7c7222b1cb82277f952e
GLENN-LAPTOP\glenn <glennrkeates@gmail.com>
2018-07-09 13:09:04 +0100

    add mac python to path

--- .travis/install.sh
+++ .travis/install.sh
@@ -5,6 +5,7 @@ if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
     brew outdates xctool || brew upgrade xctool
     brew upgrade python
     pip3 install --user pyqt5 requests pytube imageio pyinstaller git+https://github.com/RebelliousPebble/moviepy.git
+    export PATH=$PATH:/Users/travis/Library/Python/3.7/bin
 else
     sudo apt-get install python3 python3-pip
     pip3 install requests pytube imageio pyinstaller pyqt5 sip git+https://github.com/RebelliousPebble/moviepy.git
