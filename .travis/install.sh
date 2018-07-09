c3d074317a94d3ebc156715efc2066ac194ea2c2
GLENN-LAPTOP\glenn <glennrkeates@gmail.com>
2018-07-09 10:05:25 +0100

    add pyinstaller

--- .travis/install.sh
+++ .travis/install.sh
@@ -6,9 +6,9 @@ if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
     brew install python3
     sudo pip3 install pyqt5 moviepy requests pytube imageio pyinstaller
 else
-    sudo apt get install python36
+    sudo apt-get install python3
     sudo apt-get install python3-pyqt5
-    sudo pip install requests pytube imageio pyinstaller
+    sudo pip3 install requests pytube imageio pyinstaller
 fi
 
 
