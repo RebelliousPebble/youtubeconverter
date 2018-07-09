8edab59c630b6bf71d502314dee9238233d40c31
GLENN-LAPTOP\glenn <glennrkeates@gmail.com>
2018-07-09 10:17:10 +0100

    fix pyqt5

--- .travis/install.sh
+++ .travis/install.sh
@@ -4,11 +4,10 @@ if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
     brew update
     brew outdates xctool || brew upgrade xctool
     brew install python3
-    sudo pip3 install pyqt5 moviepy requests pytube imageio pyinstaller
+    pip3 install pyqt5 moviepy requests pytube imageio pyinstaller
 else
     sudo apt-get install python3 python3-pip
-    sudo apt-get install python3-pyqt5
-    sudo pip3 install requests pytube imageio pyinstaller
+    pip3 install requests pytube imageio pyinstaller pyqt5
 fi
 
 
